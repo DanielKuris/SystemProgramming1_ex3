8c1521df207af853a4aada1921676a5f087419d2
DanielKuris <danielkuris6@gmail.com>
2024-02-26 10:51:08 +0200

    Added the input reader function

--- main.c
+++ main.c
@@ -3,6 +3,45 @@
 #include "StrList.c"
 #define SIZE 10
 
+// Helper function to get story input
+// Reading input of unknown length of strings
+// Running until recieved "words" amount of words.
+char *inputString(size_t words) {
+
+    size_t size = 10; // Start with some initial size
+    char *str = (char *)malloc(size * sizeof(char));
+    if (str == NULL) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t len = 0;
+    size_t wordCount = 0;
+    int ch;
+    while (wordCount < words) {
+        ch = getchar();
+        if (ch == ' ' || ch == '\n') {
+            if (len > 0) {
+                str[len++] = '\0'; // Null-terminate the word
+                wordCount++;
+                len = 0; // Reset the length for the next word
+            }
+        } else {
+            str[len++] = ch;
+            if (len == size) { //Increasing memory allocation if reached the previous one
+                size *= 2;
+                str = (char *)realloc(str, size * sizeof(char));
+                if (str == NULL) {
+                    printf("Memory reallocation failed\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+    str[len] = '\0'; // Null-terminate the last word
+    return str;
+}
+
 int main(){
 StrList* StrList= StrList_alloc();
    int input;
@@ -19,12 +58,11 @@ StrList* StrList= StrList_alloc();
     case 1:
              printf("Enter size of the array: ");
                 scanf("%d", &index);
-
-                for (int i = 0; i < index; i++) {
-                    printf("Enter string: ");
-                    scanf(" %s", str);
-                    StrList_insertLast(StrList, str);
-                }
+            
+             char *str = inputString(index); //Get input using helper function
+             StrList_insertLast(StrList, str);
+             free(str)
+             
         scanf(" %d",&input);
         break;
     
